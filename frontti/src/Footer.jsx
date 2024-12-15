2c4baacbd474765edace26603cd875278fd54ca5
Riitahuhta <juho.riitahuhta@gmail.com>
2024-12-15 20:21:13 +0200

    Lisätty Footer, Header, MainBody tiedostot ja tehty suunnitelmaa miten ohjelma toteutetaan

---
+++ frontti/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <div>
+        <p>Tallennetut kurssit ja muistiinpanot tänne.</p>
+      </div>
+    );
+  }
+  
+  export default Footer;
---
+++ frontti/src/MainBody.jsx
@@ -0,0 +1,11 @@
+function MainBody() {
+    return (
+      <div>
+        <p><h3>Add new notes for course</h3></p>
+        <p>Tekstikenttä johon laitetaan kurssin nimi</p>
+        <p>Isompi Tekstikenttä johon kirjoitetaan kurssista sisältöä</p>
+      </div>
+    );
+  }
+  
+  export default MainBody;
