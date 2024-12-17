228e53f78e22d60677c6a2fc4f0ad72c72a49a5c
Juho Riitahuhta <juho.riitahuhta@gmail.com>
2024-12-17 15:36:54 +0200

    Lisätty Header, Footer ja MainBody + pientä softan suunnittelua

---
+++ frontti/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            <p>Tallennetut kurssit näkyy täällä</p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ frontti/src/MainBody.jsx
@@ -0,0 +1,10 @@
+function MainBody() {
+    return (
+      <div>
+        <p>Tähän tulee kohta missä voi laittaa kurssin</p>
+        <p>Tähän tulee boxi ja tekstikenttä johon tulee muistiinpanot</p>
+      </div>
+    );
+  }
+  
+  export default MainBody;
