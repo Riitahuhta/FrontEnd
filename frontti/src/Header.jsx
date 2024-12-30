057e7bdb1da919d000c2155721ffd0b8df7dbc7f
Juho Riitahuhta <juho.riitahuhta@gmail.com>
2024-12-30 18:50:42 +0200

    Muokattiin Headeriä

--- frontti/src/Header.jsx
+++ frontti/src/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     <header className="bg-black text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Megasofta
+          Muistiinpano sovellus
         </h1>
         <nav className="flex space-x-4">
           <Link className="hover:underline" to="/">
